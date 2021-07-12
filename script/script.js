2d153dfe77fba58f3b307b35ff7ae33d905df10a
Rustam <himynameisernie@yandex.ru>
2021-07-12 13:48:34 +0300

    First commit

---
+++ script/script.js
@@ -0,0 +1,15 @@
+// function getTable(number) {
+//     let table = '';
+
+//     for (let i = 1; i <= 10; i++) {
+//         table += `${i} * ${number} = ${i*number}${i < 10 ? '\n' : ''}`;
+//     };
+
+//     return table;
+// };
+
+// const mouthSize = (animal) => animal.toLowerCase() == "alligator" ? "small" : "wide";
+
+
+
+
